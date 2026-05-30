a91ab06e5772624c8be82a123824049d778e1826
Efrata Habte <efratahabte1213@gmail.com>
2026-05-30 20:26:31 +0300

    feat: Implement SSTF disk scheduling algorithm

--- SSTF_Disk_Scheduling.py
+++ SSTF_Disk_Scheduling.py
@@ -1,13 +1,13 @@
-from collections import deque
 
 # Assumptions : 
     # 1. 1 sector movement = 1 time unit
     # 2. service time = seek time
     # 3. one process has exactly one sector request
 
-pending_requests = deque()
+pending_requests = []
 waiting_time= []
 valid_requests = []
+service_order = []
 seek_time = 0
 total_waiting_time = 0
 ave_seek_distance = 0
@@ -31,31 +31,52 @@ for req in pending_requests:
         print(f"process {req[0]} -> sector {req[1]} :  Invalid request sector")
         continue
     valid_requests.append(req)
-    waiting_time.append(seek_time)
-    seek_time += abs(current_pos - req[1])
-    current_pos = req[1]
 
 
-num_valid_req = len(valid_requests)
+def nearest(requests, current_pos):
+    min_diff = sector*2
+    ans = requests[0]
     
-if num_valid_req > 0:
-    total_waiting_time = sum(waiting_time)
-    ave_waiting_time = total_waiting_time / num_valid_req
-    ave_seek_distance = seek_time / num_valid_req
-    throughput = num_valid_req / seek_time
+    for req in requests:
+        diff = abs(current_pos - req[1])
+        if min_diff >= diff:
+            ans = req
+            min_diff = diff
+    
+    return ans
+    
+n = len(valid_requests)
 
+while True:
+    if len(valid_requests) == 0:
+        break
+    
+    near = nearest(valid_requests,current_pos)
+    service_order.append(near)
+    waiting_time.append(seek_time)
+    seek_time += abs(current_pos - near[1])
+    valid_requests.remove(near)
+    current_pos = near[1]
+
+if n > 0:
+    ave_seek_distance = seek_time / n
+    total_waiting_time = sum(waiting_time)
+    ave_waiting_time = total_waiting_time / n
+
+if seek_time > 0:
+    throughput = n / seek_time 
 
 print("")
 print("Request Order: ")
-for i in range(num_valid_req):
-    print(f"{valid_requests[i]}", end ="->")
+for i in range(n):
+    print(f"{service_order[i][1]}", end ="->")
 
 print(f"Total head movement : {seek_time}")
 print(f"Average seek distance : {ave_seek_distance}")
 print(f"Throughput : {throughput} request/time-unit")
 
-for i in range(num_valid_req):
-    print(f"Waiting time for process {valid_requests[i][0]} : {waiting_time[i]}")
+for i in range(n):
+    print(f"Waiting time for process {service_order[i][0]} : {waiting_time[i]}")
 
 print(f"Total waiting time: {total_waiting_time}")
 print(f"Average waiting time : {ave_waiting_time}")
